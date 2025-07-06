f1835113467e764efdcdd280efd484e9c08bf9b2
Simon Li <orpheus+devel@gmail.com>
2025-07-06 22:00:41 +0100

    Run copy-home-config.sh in start-tigervnc.sh
    start-tigervnc.sh may be the entrypoint

--- start-tigervnc.sh
+++ start-tigervnc.sh
@@ -4,4 +4,6 @@ set -eu
 XSTARTUP=/opt/conda/lib/python3.12/site-packages/jupyter_remote_desktop_proxy/share/xstartup
 LOCALHOST_ARGS="-localhost no --I-KNOW-THIS-IS-INSECURE"
 
+sh /usr/local/bin/before-notebook.d/copy-home-config.sh
+
 /usr/bin/tigervncserver :1 -fg $LOCALHOST_ARGS -SecurityTypes None -xstartup "$XSTARTUP"
